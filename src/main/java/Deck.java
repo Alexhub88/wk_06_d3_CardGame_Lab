702422606c7f8b71773666a4fd87d9627446d540
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-11 14:26:19 +0100

    Deck class created with populate, shuffle and deal methods. Tested and show to be successful.

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,5 +1,36 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
-    ArrayList<Card> cards = new ArrayList<>();
+    private ArrayList<Card> cards;
+
+    public Deck() {
+         cards = new ArrayList<>();
+    }
+
+    public int deckSize(){
+        return cards.size();
+    }
+
+    public void populate(){
+        for (Suit suit: Suit.values()){
+            for (Rank rank: Rank.values()){
+                cards.add(new Card(suit, rank));
+            }
+        }
+    }
+
+    public ArrayList<Card> getDeckCards(){
+        return cards;
+    }
+
+    public void shuffle(){
+        Collections.shuffle(cards);
+    }
+
+    public Card dealCard(){
+        return cards.remove(0);
+    }
+
+
 }
---
+++ src/test/java/DeckTest.java
@@ -0,0 +1,54 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeckTest {
+    private Deck deck1;
+    private Deck deck2;
+
+    @Before
+    public void before(){
+        deck1 = new Deck();
+        deck2 = new Deck();
+    }
+
+    @Test
+    public void hasZeroCards(){
+        assertEquals(0, deck1.deckSize());
+
+    }
+
+    @Test
+    public void hasFullDeck(){
+        deck1.populate();
+        assertEquals(52, deck1.deckSize());
+    }
+
+    @Test
+    public void cardsPopulated(){
+        deck1.populate();
+        assertEquals(Suit.HEARTS, ((deck1.getDeckCards()).get(2)).getSuit());
+        assertEquals(3, ((deck1.getDeckCards()).get(2)).getRankValue());
+    }
+
+    @Test
+    public void hasShuffledDeck(){
+        deck1.populate();
+        deck2.populate();
+        deck1.shuffle();
+        assertEquals(false, deck1 == deck2);
+    }
+
+    @Test
+    public void canDealCard(){
+        deck1.populate();
+        deck1.dealCard();
+        assertEquals(51, deck1.deckSize());
+        assertEquals(2, ((deck1.getDeckCards()).get(0)).getRankValue());
+    }
+
+    
+}
